645778800cbdd9f59ed710295b730802514f045f
Zac Nowicki <zachnowicki@gmail.com>
2018-05-28 05:56:10 -0400

    [REST] Add endpoint descriptions

---
+++ spec/twitch/request_spec.cr
@@ -0,0 +1,157 @@
+require "../spec_helper"
+
+describe Twitch::Request do
+  it ".get_bits_leaderboard" do
+    time = Time.new(2018, 1, 1)
+    request = Twitch::Request.get_bits_leaderboard(
+      count: 10,
+      period: Twitch::Request::Period::Day,
+      started_at: time,
+      user_id: 123)
+
+    time_str = URI.escape(Time::Format::ISO_8601_DATE_TIME.format(time))
+    request.method.should eq "GET"
+    request.path.should eq "/helix/bits/leaderboard"
+    request.query.should eq "count=10&period=day&started_at=#{time_str}&user_id=123"
+  end
+
+  it ".create_clip" do
+    request = Twitch::Request.create_clip(
+      broadcaster_id: 123,
+      has_delay: true)
+
+    request.method.should eq "POST"
+    request.path.should eq "/helix/clips"
+    request.query.should eq "broadcaster_id=123&has_delay=true"
+  end
+
+  it ".get_clips" do
+    request = Twitch::Request.get_clips(
+      broadcaster_id: 123,
+      game_id: 456,
+      id: ["foo", "bar"])
+
+    request.method.should eq "GET"
+    request.path.should eq "/helix/clips"
+    request.query.should eq "broadcaster_id=123&game_id=456&id=foo&id=bar"
+  end
+
+  it ".create_entitlement_url" do
+    request = Twitch::Request.create_entitlement_url(
+      manifest_id: "foo",
+      type: "bar")
+
+    request.method.should eq "POST"
+    request.path.should eq "/helix/entitlements/upload"
+    request.query.should eq "manifest_id=foo&type=bar"
+  end
+
+  it ".get_games" do
+    request = Twitch::Request.get_games(
+      id: 123,
+      name: ["foo", "bar"])
+
+    request.method.should eq "GET"
+    request.path.should eq "/helix/games"
+    request.query.should eq "id=123&name=foo&name=bar"
+  end
+
+  it ".get_game_analytics" do
+    request = Twitch::Request.get_game_analytics(game_id: 123)
+
+    request.method.should eq "GET"
+    request.path.should eq "/helix/analytics/games"
+    request.query.should eq "game_id=123"
+  end
+
+  it ".get_top_games" do
+    request = Twitch::Request.get_top_games(
+      after: "foo",
+      before: "bar",
+      first: 1)
+
+    request.method.should eq "GET"
+    request.path.should eq "/helix/games/top"
+    request.query.should eq "after=foo&before=bar&first=1"
+  end
+
+  it ".get_streams" do
+    request = Twitch::Request.get_streams(
+      after: "foo",
+      before: "bar",
+      community_id: "baz",
+      first: 1,
+      game_id: [2, 3],
+      language: "bar",
+      user_id: 123,
+      user_login: "baz")
+
+    request.method.should eq "GET"
+    request.path.should eq "/helix/streams"
+    request.query.should eq "after=foo&before=bar&community_id=baz&first=1&game_id=2&game_id=3&language=bar&user_id=123&user_login=baz"
+  end
+
+  it ".get_stream_metadata" do
+    request = Twitch::Request.get_stream_metadata(
+      after: "foo",
+      before: "bar",
+      community_id: "baz",
+      first: 1,
+      game_id: [2, 3],
+      language: "bar",
+      user_id: 123,
+      user_login: "baz")
+
+    request.method.should eq "GET"
+    request.path.should eq "/helix/streams/metadata"
+    request.query.should eq "after=foo&before=bar&community_id=baz&first=1&game_id=2&game_id=3&language=bar&user_id=123&user_login=baz"
+  end
+
+  it ".get_users" do
+    request = Twitch::Request.get_users(
+      id: 1,
+      login: ["foo", "bar"])
+
+    request.method.should eq "GET"
+    request.path.should eq "/helix/users"
+    request.query.should eq "id=1&login=foo&login=bar"
+  end
+
+  it ".get_user_follows" do
+    request = Twitch::Request.get_user_follows(
+      after: "foo",
+      first: 1,
+      from_id: 2,
+      to_id: 3)
+
+    request.method.should eq "GET"
+    request.path.should eq "/helix/users/follows"
+    request.query.should eq "after=foo&first=1&from_id=2&to_id=3"
+  end
+
+  it ".update_user" do
+    request = Twitch::Request.update_user("foo")
+
+    request.method.should eq "PUT"
+    request.path.should eq "/helix/users"
+    request.query.should eq "description=foo"
+  end
+
+  it ".get_videos" do
+    request = Twitch::Request.get_videos(
+      id: [1, 2],
+      user_id: 3,
+      game_id: 4,
+      after: "after",
+      before: "before",
+      first: 1,
+      language: "language",
+      period: Twitch::Request::Period::Day,
+      sort: Twitch::Request::Sort::Trending,
+      type: Twitch::Request::VideoType::Upload)
+
+    request.method.should eq "GET"
+    request.path.should eq "/helix/videos"
+    request.query.should eq "id=1&id=2&user_id=3&game_id=4&after=after&before=before&first=1&language=language&period=day&sort=trending&type=upload"
+  end
+end
---
+++ src/twitch/requests.cr
@@ -0,0 +1,282 @@
+# :nodoc:
+module Twitch::Request
+  extend self
+
+  enum Period
+    Day
+    Week
+    Month
+    Year
+    All
+  end
+
+  def get_bits_leaderboard(count : Int32?, period : Period?, started_at : Time?,
+                           user_id : Int32?)
+    params = HTTP::Params.build do |form|
+      form.add("count", count.to_s) if count
+      form.add("period", period.to_s.downcase) if period
+      form.add("started_at", Time::Format::ISO_8601_DATE_TIME.format(started_at)) if started_at
+      form.add("user_id", user_id.to_s) if user_id
+    end
+
+    HTTP::Request.new("GET", "/helix/bits/leaderboard?#{params}")
+  end
+
+  def create_clip(broadcaster_id : Int32, has_delay : Bool? = nil)
+    params = HTTP::Params.build do |form|
+      form.add("broadcaster_id", broadcaster_id.to_s)
+      form.add("has_delay", has_delay.to_s) if !has_delay.nil?
+    end
+
+    HTTP::Request.new("POST", "/helix/clips?#{params}")
+  end
+
+  def get_clips(broadcaster_id : Int32?, game_id : Int32?, id : String | Array(String)?)
+    params = HTTP::Params.build do |form|
+      form.add("broadcaster_id", broadcaster_id.to_s) if broadcaster_id
+      form.add("game_id", game_id.to_s) if game_id
+      if id.is_a?(Array)
+        id.each do |value|
+          form.add("id", value)
+        end
+      elsif id.is_a?(String)
+        form.add("id", id)
+      end
+    end
+
+    HTTP::Request.new("GET", "/helix/clips?#{params}")
+  end
+
+  def create_entitlement_url(manifest_id : String, type : String)
+    params = HTTP::Params.build do |form|
+      form.add("manifest_id", manifest_id)
+      form.add("type", type)
+    end
+
+    HTTP::Request.new("POST", "/helix/entitlements/upload?#{params}")
+  end
+
+  def get_games(id : Int32 | Array(Int32)?, name : String | Array(String)?)
+    params = HTTP::Params.build do |form|
+      if id.is_a?(Array)
+        id.each do |value|
+          form.add("id", value.to_s)
+        end
+      elsif id.is_a?(Int32)
+        form.add("id", id.to_s)
+      end
+
+      if name.is_a?(Array)
+        name.each do |value|
+          form.add("name", value)
+        end
+      elsif name.is_a?(String)
+        form.add("name", name)
+      end
+    end
+
+    HTTP::Request.new("GET", "/helix/games?#{params}")
+  end
+
+  def get_game_analytics(game_id : Int32?)
+    params = HTTP::Params.build do |form|
+      form.add("game_id", game_id.to_s) if game_id
+    end
+
+    HTTP::Request.new("GET", "/helix/analytics/games?#{params}")
+  end
+
+  def get_top_games(after : String?, before : String?, first : Int32?)
+    params = HTTP::Params.build do |form|
+      form.add("after", after) if after
+      form.add("before", before) if before
+      form.add("first", first.to_s) if first
+    end
+
+    HTTP::Request.new("GET", "/helix/games/top?#{params}")
+  end
+
+  def get_streams(after : String?, before : String?, community_id : String | Array(String)?,
+                  first : Int32?, game_id : Int32 | Array(Int32)?, language : String | Array(String)?,
+                  user_id : Int32 | Array(Int32)?, user_login : String | Array(String)?)
+    params = HTTP::Params.build do |form|
+      form.add("after", after) if after
+      form.add("before", before) if before
+
+      if community_id.is_a?(Array)
+        community_id.each do |id|
+          form.add("community_id", id)
+        end
+      elsif community_id.is_a?(String)
+        form.add("community_id", community_id)
+      end
+
+      form.add("first", first.to_s) if first
+
+      if game_id.is_a?(Array)
+        game_id.each do |id|
+          form.add("game_id", id.to_s)
+        end
+      elsif game_id.is_a?(Int32)
+        form.add("game_id", game_id.to_s)
+      end
+
+      if language.is_a?(Array)
+        language.each do |lang|
+          form.add("language", lang)
+        end
+      elsif language.is_a?(String)
+        form.add("language", language)
+      end
+
+      if user_id.is_a?(Array)
+        user_id.each do |id|
+          form.add("user_id", id.to_s)
+        end
+      elsif user_id.is_a?(Int32)
+        form.add("user_id", user_id.to_s)
+      end
+
+      if user_login.is_a?(Array)
+        user_login.each do |user|
+          form.add("user_login", user)
+        end
+      elsif user_login.is_a?(String)
+        form.add("user_login", user_login)
+      end
+    end
+
+    HTTP::Request.new("GET", "/helix/streams?#{params}")
+  end
+
+  def get_stream_metadata(after : String?, before : String?, community_id : String | Array(String)?,
+                          first : Int32?, game_id : Int32 | Array(Int32)?, language : String? | Array(String)?,
+                          user_id : Int32 | Array(Int32)?, user_login : String | Array(String)?)
+    params = HTTP::Params.build do |form|
+      form.add("after", after) if after
+      form.add("before", before) if before
+
+      if community_id.is_a?(Array)
+        community_id.each do |id|
+          form.add("community_id", id)
+        end
+      elsif community_id.is_a?(String)
+        form.add("community_id", community_id)
+      end
+
+      form.add("first", first.to_s) if first
+
+      if game_id.is_a?(Array)
+        game_id.each do |id|
+          form.add("game_id", id.to_s)
+        end
+      elsif game_id.is_a?(Int32)
+        form.add("game_id", game_id.to_s)
+      end
+
+      if language.is_a?(Array)
+        language.each do |lang|
+          form.add("language", lang)
+        end
+      elsif language.is_a?(String)
+        form.add("language", language)
+      end
+
+      if user_id.is_a?(Array)
+        user_id.each do |id|
+          form.add("user_id", id.to_s)
+        end
+      elsif user_id.is_a?(Int32)
+        form.add("user_id", user_id.to_s)
+      end
+
+      if user_login.is_a?(Array)
+        user_login.each do |user|
+          form.add("user_login", user)
+        end
+      elsif user_login.is_a?(String)
+        form.add("user_login", user_login)
+      end
+    end
+
+    HTTP::Request.new("GET", "/helix/streams/metadata?#{params}")
+  end
+
+  def get_users(id : Int32 | Array(Int32)?, login : String | Array(String)?)
+    params = HTTP::Params.build do |form|
+      if id.is_a?(Array)
+        id.each do |value|
+          form.add("id", value.to_s)
+        end
+      elsif id.is_a?(Int32)
+        form.add("id", id.to_s)
+      end
+
+      if login.is_a?(Array)
+        login.each do |value|
+          form.add("login", value)
+        end
+      elsif login.is_a?(String)
+        form.add("login", login)
+      end
+    end
+
+    HTTP::Request.new("GET", "/helix/users?#{params}")
+  end
+
+  def get_user_follows(after : String?, first : Int32?, from_id : Int32?,
+                       to_id : Int32?)
+    params = HTTP::Params.build do |form|
+      form.add("after", after) if after
+      form.add("first", first.to_s) if first
+      form.add("from_id", from_id.to_s) if from_id
+      form.add("to_id", to_id.to_s) if to_id
+    end
+
+    HTTP::Request.new("GET", "/helix/users/follows?#{params}")
+  end
+
+  def update_user(description : String)
+    HTTP::Request.new("PUT", "/helix/users?description=#{description}")
+  end
+
+  enum Sort
+    Time
+    Trending
+    Views
+  end
+
+  enum VideoType
+    All
+    Upload
+    Archive
+    Highlight
+  end
+
+  def get_videos(id : Int32 | Array(Int32)?, user_id : Int32?, game_id : Int32?,
+                 after : String?, before : String?, first : Int32?,
+                 language : String?, period : Period?, sort : Sort?,
+                 type : VideoType?)
+    params = HTTP::Params.build do |form|
+      if id.is_a?(Array)
+        id.each do |value|
+          form.add("id", value.to_s)
+        end
+      elsif id.is_a?(Int32)
+        form.add("id", id.to_s)
+      end
+
+      form.add("user_id", user_id.to_s) if user_id
+      form.add("game_id", game_id.to_s) if game_id
+      form.add("after", after) if after
+      form.add("before", before) if before
+      form.add("first", first.to_s) if first
+      form.add("language", language)
+      form.add("period", period.to_s.downcase) if period
+      form.add("sort", sort.to_s.downcase) if period
+      form.add("type", type.to_s.downcase) if type
+    end
+
+    HTTP::Request.new("GET", "/helix/videos?#{params}")
+  end
+end

ee622af3cf3556aae4572ffa2a7bd13ab19cf323
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2019-09-20 14:32:47 +0100

    Fixup: Stream Id Converter

--- src/twitch/mappings/converters.cr
+++ src/twitch/mappings/converters.cr
@@ -7,3 +7,13 @@ module Twitch::IDConverter
     builder.scalar(value.to_s)
   end
 end
+
+module Twitch::ID64Converter
+  def self.from_json(parser : JSON::PullParser)
+    parser.read_string.to_i64
+  end
+
+  def self.to_json(value : Int64, builder : JSON::Builder)
+    builder.scalar(value.to_s)
+  end
+end
--- src/twitch/mappings/stream.cr
+++ src/twitch/mappings/stream.cr
@@ -1,11 +1,11 @@
 struct Twitch::Stream
   JSON.mapping(
-    id: {type: Int32, converter: IDConverter},
+    id: {type: Int64, converter: ID64Converter},
     user_id: {type: Int32, converter: IDConverter},
     user_name: String,
     game_id: {type: Int32, converter: IDConverter},
     community_ids: Array(String),
-    type: String,
+    type: String, # TODO: Enum
     title: String,
     viewer_count: Int32,
     started_at: String, # TODO: Time
